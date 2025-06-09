7aaa5a28dd4c442e396690afc4d4afaab9eac649
hongjm0912~ <s25036@gsm.hs.kr>
2025-06-09 20:48:22 +0900

    로그인 회원가입

--- src/main/java/com/example/eum2/member/controller/MemberController.java
+++ src/main/java/com/example/eum2/member/controller/MemberController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:63342")
-@Controller
+@RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class MemberController {
@@ -25,22 +25,6 @@ public class MemberController {
     private final MemberService memberService;
     private final JWTUtil jwtUtil;
 
-    // 회원가입 폼 페이지
-    @GetMapping("/save")
-    public String saveForm() {
-        return "save"; // templates/save.html
-    }
-
-    // HTML Form 회원가입 처리
-    @PostMapping("/save")
-    public String save(@ModelAttribute UserSignupDTO dto) {
-        UserEntity user = new UserEntity();
-        user.setStudent_name(dto.getStudent_name());
-        user.setEmail(dto.getEmail().toLowerCase());  // 이메일 소문자 저장 필수
-        user.setPassword(dto.getPassword());
-        userRepository.save(user);
-        return "index"; // templates/index.html
-    }
 
     // API 회원가입 (JSON 요청)
     @PostMapping("/signup")
