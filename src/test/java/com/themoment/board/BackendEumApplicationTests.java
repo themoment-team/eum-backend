c641d57340ea7b7e82a5dfcb432fc6c28ad6e98f
ZaMan <roblery128@gmail.com>
2025-06-09 21:01:51 +0900

    HomeController 삭제

--- src/test/java/com/themoment/board/BackendEumApplicationTests.java
+++ src/test/java/com/themoment/board/BackendEumApplicationTests.java
@@ -1,4 +1,4 @@
-package com.study.backendeum;
+package com.themoment.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/com/themoment/login/Eum2ApplicationTests.java
+++ src/test/java/com/themoment/login/Eum2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eum2;
+package com.themoment.login;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
